49b3e1234b7f46e78980143b20e7e212a7e1642e
Dawid Starkowski <dawid.starkowski@hotmail.com>
2016-05-21 17:21:20 +0100

    SharePoint context data loaded from tabs to extension #11

--- Firefox/lib/index.tsx
+++ Firefox/lib/index.tsx
@@ -1,5 +1,6 @@
 declare function require(name: string): any;
 
+import { ISPPageContext, ISPListContext, ITabContext, TabContextCollection } from './interfaces';
 import { NavigationCommand, NavigationCommands } from './commands';
 import { PanelHandler } from './panel-handler';
 
@@ -8,19 +9,41 @@ var Tabs = require('sdk/tabs');
 class Extension {
 	private _modifier: string;
 	private _panel: PanelHandler;
+	private _tabContext: TabContextCollection;
 
 	constructor() {
 		this._modifier = '';
+		this._tabContext = {};
 		this._panel = new PanelHandler();
-		this._panel.on('text-entered', (text: string) => this.onTextEntered(text)); 
+		this._panel.on('text-entered', (text: string) => this.onTextEntered(text));
 		this._panel.on('text-changed', (text: string) => this.onTextChanged(text));
+
+		Tabs.on('ready', (tab: any) => this.onTabReady(tab));
+	}
+	
+	private onTabReady(tab: any) {
+		let worker = tab.attach({
+			contentScriptFile: './context.js',
+			contentScriptOptions: { tabId: tab.id }
+		});
+
+		worker.port.on('context-ready', (context : ITabContext) => this.onContextReady(context));
+		worker.port.emit('load-context');
+	}
+	
+	private onContextReady(context: ITabContext) {
+		this._tabContext[context.tabId] = context;
 	}
 	
 	private onTextEntered(commandText: string) {
 		var navigationCommand = NavigationCommands.getCommand(commandText);
 
 		if (typeof (navigationCommand) != 'undefined') {
-			this.navigate(navigationCommand.scope, navigationCommand.url);
+			var tab = Tabs.activeTab;
+			var context = this._tabContext[tab.id];
+			
+			var webUrl = this.getScopeUrl(navigationCommand.scope, context);
+			tab.url = webUrl + navigationCommand.url;
 		}
 
 		this._panel.toggle();
@@ -32,16 +55,23 @@ class Extension {
 		this._panel.emit('suggestions-ready', commands);
 	}
 
-	private invokeScript(scriptFile: string, options: any) {
-		Tabs.activeTab.attach({
+	private invokeScript(tab: any, scriptFile: string, options: any) : any {
+		return tab.attach({
 			contentScriptFile: scriptFile,
 			contentScriptOptions: options
 		});
 	}
 
-	private navigate(scope: string, url: string) {
-		var options = { 'scope': scope, 'url': url };
-		this.invokeScript('./navigate.js', options);
+	private getScopeUrl(scope: string, context: ITabContext) {
+		if (scope.toLowerCase() == 'web') {
+			return context.spPageContextInfo.webAbsoluteUrl;
+		}
+
+		if (scope.toLowerCase() == 'site') {
+			return context.spPageContextInfo.siteAbsoluteUrl;
+		}
+		
+		return ''; 
 	}
 }
 
---
+++ Firefox/lib/interfaces.ts
@@ -0,0 +1,32 @@
+export interface ISPPageContext {
+	webServerRelativeUrl: string; // /site
+	webAbsoluteUrl: string; // https://tenant.sharepoint.com/site
+	siteAbsoluteUrl: string; // https://tenant.sharepoint.com/site
+	serverRequestPath: string; // /_layouts/15/settings.aspx
+	layoutsUrl: string; // _layouts/15
+	isAppWeb: boolean;
+	webLanguage: string; // 1033
+	currentLanguage: string; // 1033
+	currentUICultureName: string; // en-US
+	currentCultureName: string; // en-US
+	webUIVersion: string; // 15
+	siteServerRelativeUrl: string;
+	ProfileUrl: string; // https://tenant-my.sharepoint.com/person.aspx
+}
+
+export interface ISPListContext {
+	listName: string; // {00000000-0000-0000-0000-0000000000000},
+	ListTitle: string;
+	listUrlDir: string; // /site/lists/listUrl
+}
+
+export interface ITabContext {
+	isSharePoint : boolean;
+	tabId : any;
+	spPageContextInfo : ISPPageContext;
+	ctx : ISPListContext;
+}
+
+export interface TabContextCollection {
+    [key: string]: ITabContext;
+}
